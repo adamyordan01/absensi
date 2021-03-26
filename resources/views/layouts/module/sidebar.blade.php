e2c2aeaf36d48d60825d3e277bb3e02db67f5451
adamyordan01 <adamyordan0102@gmail.com>
2021-03-26 07:18:03 +0700

    Renew Home view

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -1,7 +1,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
         <div class="sidebar-brand-icon">
             <img src="{{ asset('assets') }}/img/BPNLangsa.png" width="50" height="50" alt="">
         </div>
@@ -16,18 +16,18 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('dashboard') }}">
+        <a class="nav-link" href="#">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
 
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('product.index') }}">
+        <a class="nav-link" href="#">
             <i class="fas fa-fw fa-box-open"></i>
             <span>Barang</span></a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('category.index') }}">
+        <a class="nav-link" href="#">
             <i class="fas fa-fw fa-boxes"></i>
             <span>Kategori Barang</span></a>
     </li>
